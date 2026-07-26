e2d77cc0e792d55151328601003877006817b7e1
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-07-26 18:20:58 +1200

    Working with lists(Task 1)

---
+++ Working with lists( Task 1).py
@@ -0,0 +1,13 @@
+'''
+  date: 26/07/2026
+  author: Frank Zhang
+  description: Working with lists (Task 1)
+  version: 1.0
+'''
+#This is a list, lists always start counting from 0
+student_list = ["Jenna", "Bob", "Tim", "Greg", "Jimmy"]
+print(student_list[0])
+print(student_list[1])
+print(student_list[2])
+print(student_list[3])
+print(student_list[4])
