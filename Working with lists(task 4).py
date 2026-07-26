995425707ae751e220a42011f68c84cb719d2825
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-07-26 22:57:41 +1200

    Working with lists( task 4)

---
+++ Working with lists(task 4).py
@@ -0,0 +1,15 @@
+'''
+  date: 26/07/2026
+  author: Frank Zhang
+  description: Working with lists (Task 3)
+  version: 1.0
+'''
+
+#This is a list, lists always start counting from 0
+student_list = ["Jenna", "Bob", "Tim", "Greg", "Jimmy", "Frank", "Sally", "Tom"]
+count = 0 #Creat a count
+while count < len(student_list):  #while 0 is less than the length of the list
+    print(count, student_list[count])  #print a number, access the list
+    count += 1  #the count must increment by 1 to acess the next name on the list
+    # outputs the total number of students
+    print("That is all of the students, {} of them in total".format(count))
