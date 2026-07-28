9313b56e7bbd53d327c47dd652dc17da17d80898
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-07-28 22:46:39 +1200

    Happy Birthday task

---
+++ Happy birthday task.py
@@ -0,0 +1,14 @@
+'''
+  date: 29/07/2026
+  author: Frank Zhang
+  description: Happy birthday task
+  version: 1.0
+'''
+
+def happy_birthday():
+    print("Happy birthday to you.\nHappy birthday to you.\nHappy birthday. Happy birthday.\nHappy birthday to you.\n")
+
+happy_birthday()
+happy_birthday()
+print("Happy birthday,\nhappy birthday,\nhappy birthday,\nto you.\n")
+happy_birthday()
