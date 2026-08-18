73b3745ba4afad388ba83d466e76ab56f88eeebb
frankz24283-dev <frankz.24283@obhs.school.nz>
2026-08-19 09:11:56 +1200

    Start
    Structure of the code

---
+++ guessing_games.py
@@ -0,0 +1,12 @@
+'''
+   author: Frank Zhang
+   date: 19/08/2026
+   version: 1.0
+   description: Guess a number between 1-100
+'''
+
+#-------libraries---------
+
+#------functions---------
+
+#------main routine------
